3300a9086c6919d9cd91ad26c94230a1a5558553
Roberto <robertoperalta@gmail.com>
2024-07-11 19:09:31 +0200

    Casi terminado a falta de responsive

--- js/index.js
+++ js/index.js
@@ -1,29 +1,16 @@
-console.log('Cargado script...');
-
-// function scrollFunction(){
-//   console.log("Scroll");
-// }
-
-// window.onscroll = scrollFunction;
-
 window.addEventListener('load', function() {
   var header = document.getElementById('header');
   var hero = document.getElementById('hero');
 
-  console.log('DOM completamente cargado y parseado');
-
   window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
       header.classList.add('shrink');
-      console.log('scroll..');
     } else {
       header.classList.remove('shrink');
-      console.log('Header expandido');
     }
 
     // Manejar el efecto parallax
     var scrollPosition = window.scrollY;
     hero.style.backgroundPosition = 'center ' + (-scrollPosition * 0.5) + 'px';
-    console.log('Posición del background actualizada: ' + (-scrollPosition * 0.5) + 'px');
   });
 });

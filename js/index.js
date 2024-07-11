86ca951c4118da085b8a5b8a0c86bdd65e59aa05
Roberto <robertoperalta@gmail.com>
2024-07-11 16:01:26 +0200

    Estado inicial

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.getElementById('header');
+  var hero = document.getElementById('hero');
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+      header.classList.add('shrink');
+    } else {
+      header.classList.remove('shrink');
+    }
+
+    // Manejar el efecto parallax
+    var scrollPosition = window.scrollY;
+    hero.style.backgroundPosition = 'center ' + (-scrollPosition * 0.5) + 'px';
+  });
+});

9cfc8ce59a7c686fdaac6fcfa8809cdbf5b4aa6c
Roberto <robertoperalta@gmail.com>
2024-07-24 18:54:32 +0200

    Actualiza menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 window.addEventListener('load', function() {
   var header = document.getElementById('header');
   var hero = document.getElementById('hero');
+  var menuIcon = document.getElementById('menu-icon');
+  var menu = document.getElementById('menu');
+  var menuItems = document.querySelectorAll('.menu-item');
 
+  menuIcon.addEventListener('click', function() {
+    menu.classList.toggle('show');
+    menuIcon.classList.toggle("hide");
+  });
+
+  menuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      menu.classList.remove('show');
+      menuIcon.classList.remove('hide');
+    });
+  });
+  
   window.addEventListener('scroll', function () {
     if (window.scrollY > 50) {
       header.classList.add('shrink');
